7f28cc641685db22d845817d8663345d6f7f8af1
Andrey <andreyduque1987@gmail.com>
2021-09-30 18:18:42 -0500

    Validación campos Login.

--- app/src/main/java/com/am2/eshopapp/ui/login/LoginFragment.java
+++ app/src/main/java/com/am2/eshopapp/ui/login/LoginFragment.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,6 +25,9 @@ import com.am2.eshopapp.R;
 import com.am2.eshopapp.databinding.FragmentLoginBinding;
 import com.am2.eshopapp.ui.register.RegisterFragment;
 import com.google.android.material.snackbar.Snackbar;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LoginFragment extends Fragment {
 
@@ -42,20 +46,43 @@ public class LoginFragment extends Fragment {
         binding = FragmentLoginBinding.inflate(inflater, container, false);
 
         TextView jtvSignUp = binding.tvSignUp;
-        EditText jetUsuario = binding.editTextEmail;
+        Button jbtnLogin = binding.btnLogin;
+        EditText jetEmail = binding.etEmail;
+        EditText jetPassword = binding.etPassword;
+
         jtvSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                switch (view.getId()){
-                    case R.id.tvSignUp:
-                        findNavController(view).navigate(R.id.fragmentRegister);
-                        break;
-                    case R.id.cirLoginButton:
-                        String usuario;
-                        usuario = jetUsuario.getText().toString();
+                findNavController(view).navigate(R.id.fragmentRegister);
+            }
+        });
 
+        jbtnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email, password;
+                email = jetEmail.getText().toString();
+                password = jetPassword.getText().toString();
+
+                // Si hay campos vacios
+                if (
+                        !email.isEmpty() &&
+                                !password.isEmpty()
+                ) {
+                    // si campo email tiene el formato correcto
+                    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+                    Pattern p = Pattern.compile(emailPattern);
+                    Matcher m = p.matcher(email);
+                    boolean b = m.matches();
+                    if(b){
+                        findNavController(view).navigate(R.id.nav_home);
+                    }else{
+                        Toast.makeText(getContext(), "Email no coincide.", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(getContext(), "hay un campo vacio.", Toast.LENGTH_LONG).show();
                 }
-                //
+
             }
         });
 
