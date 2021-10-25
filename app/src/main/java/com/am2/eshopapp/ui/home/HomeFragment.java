7ec5cfda8e9c71c4c8bc6a7b5aac7bb99793071e
Andrey <andreyduque1987@gmail.com>
2021-10-25 17:30:58 -0500

    ConexionBD

--- app/src/main/java/com/am2/eshopapp/ui/home/HomeFragment.java
+++ app/src/main/java/com/am2/eshopapp/ui/home/HomeFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Adapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,7 +20,11 @@ import com.am2.eshopapp.Adapters.ProductAdapter;
 import com.am2.eshopapp.Entities.ProductEntity;
 import com.am2.eshopapp.R;
 import com.am2.eshopapp.databinding.FragmentHomeBinding;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,25 +34,27 @@ public class HomeFragment extends Fragment {
     ProductAdapter productAdapter;
     RecyclerView recyclerViewProduct;
     ArrayList<ProductEntity> listProduct;
+    private FirebaseFirestore db;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_home,container,false);
         recyclerViewProduct = view.findViewById(R.id.rvProducts);
+        db = FirebaseFirestore.getInstance();
         listProduct = new ArrayList<>();
         // Cargar la lista
-        loadList();
+        getProducts();
         // Mostrar data
         showData();
         return view;
     }
 
-    public void loadList(){
-        listProduct.add(new ProductEntity("Monitor","300","50","LG"));
-        listProduct.add(new ProductEntity("Torre","30","30","Gamer"));
-        listProduct.add(new ProductEntity("Mouse","30","30","Genius"));
-        listProduct.add(new ProductEntity("Teclado","30","30","Samsung"));
+    public void loadList(String name, String price, String stock, String description){
+//        listProduct.add(new ProductEntity("Monitor","300","50","LG"));
+//        listProduct.add(new ProductEntity("Torre","30","30","Gamer"));
+//        listProduct.add(new ProductEntity("Mouse","30","30","Genius"));
+//        listProduct.add(new ProductEntity("Teclado","30","30","Samsung"));
     }
 
     public void showData(){
@@ -61,6 +68,25 @@ public class HomeFragment extends Fragment {
 
             }
         });
+    }
+
+    public void getProducts (){
+        db.collection("products")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        if (error != null){
+                            Toast.makeText(getContext(), "Failed to retrive data", Toast.LENGTH_SHORT);
+                            return;
+                        }
+                        for (DocumentChange dc : value.getDocumentChanges()){
+                            if (dc.getType() == DocumentChange.Type.ADDED){
+                                listProduct.add(dc.getDocument().toObject(ProductEntity.class));
+                            }
+                        }
+                        productAdapter.notifyDataSetChanged();
+                    }
+                });
     }
 
     @Override
