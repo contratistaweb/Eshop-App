0ff12f04ae1843c8bd6a1fcebc3993563bbde27f
Andrey <andreyduque1987@gmail.com>
2021-10-29 12:46:50 -0500

    CRUD

--- app/src/main/java/com/am2/eshopapp/Entities/ProductEntity.java
+++ app/src/main/java/com/am2/eshopapp/Entities/ProductEntity.java
@@ -1,6 +1,10 @@
 package com.am2.eshopapp.Entities;
 
+import com.google.firebase.firestore.DocumentId;
+
 public class ProductEntity {
+    @DocumentId
+    private String id;
     private String name;
     private String price;
     private String stock;
@@ -14,6 +18,14 @@ public class ProductEntity {
         this.stock = stock;
         this.description = description;
 
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getName() {
